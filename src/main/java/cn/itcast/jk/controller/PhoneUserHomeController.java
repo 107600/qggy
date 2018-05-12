e0fdfcea1aa28173feddc9d9eed2087be0fc8005
jy107600 <jy107600@gmail.com>
2018-05-12 20:22:59 +0800

    app 端接口增加

--- src/main/java/cn/itcast/jk/controller/PhoneUserHomeController.java
+++ src/main/java/cn/itcast/jk/controller/PhoneUserHomeController.java
@@ -164,15 +164,9 @@ public class PhoneUserHomeController {
         return "/basicinfo/phoneuser/suretiqutoxianjin.html?money=" + money;
     }
 
-    //app接口转向课程列表
-    @RequestMapping("/app/user/user.action")
-    public String appToCourseList(HttpRequest request, HttpResponse response) {
-       return "/public/phone/basicinfo/course/view.action";
-    }
-
     //app接口转向个人中心
     @RequestMapping("/app/user/user.action")
-    public String appToUserCenter(HttpRequest request, HttpResponse response) {
+    public String appToUserCenter() {
        return "/basicinfo/phoneuser/Main.jsp";
     }
 
