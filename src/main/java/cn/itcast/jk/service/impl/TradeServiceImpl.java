b496709e6b2d36b3c47bb543b1cf98ef206a299f
jy107600 <jy107600@gmail.com>
2018-05-14 16:46:34 +0800

    支付提现问题解决

--- src/main/java/cn/itcast/jk/service/impl/TradeServiceImpl.java
+++ src/main/java/cn/itcast/jk/service/impl/TradeServiceImpl.java
@@ -2,6 +2,7 @@ package cn.itcast.jk.service.impl;
 
 import java.io.Serializable;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -10,9 +11,12 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Service;
 
 import cn.itcast.jk.dao.TradeDao;
+import cn.itcast.jk.dao.TradeDetailDao;
 import cn.itcast.jk.domain.Trade;
+import cn.itcast.jk.domain.TradeDetail;
 import cn.itcast.jk.pagination.Page;
 import cn.itcast.jk.service.TradeService;
+import cn.itcast.jk.service.WXPayService;
 import cn.itcast.jk.vo.TradeVO;
 
 /**
@@ -25,6 +29,10 @@ import cn.itcast.jk.vo.TradeVO;
 public class TradeServiceImpl implements TradeService {
     @Resource
     TradeDao tradeDao;
+    @Resource
+    TradeDetailDao tradeDetailDao;
+    @Resource
+    WXPayService wXPayService;
 
     @Override
     public List<Trade> findPage(Page<?> page) {
@@ -33,11 +41,63 @@ public class TradeServiceImpl implements TradeService {
 
     @Override
     public List<Trade> find(Map<?, ?> paraMap) {
-        return tradeDao.find(paraMap);
+        return tradeFilter(tradeDao.find(paraMap));
     }
 
+    // 更新数据库
+    private List<Trade> tradeFilter(List<Trade> list) {
+        Iterator<Trade> it = list.iterator();
+        while (it.hasNext()) {
+            if (tradeFilterOne(it.next())) {
+                it.remove();
+            }
+        }
+        if (list.size() == 0) {
+            return null;
+        }
+        return list;
+    }
+
+    // 更新数据库，返回true，要删除Trade
+    private boolean tradeFilterOne(Trade t) {
+        if (t.getWeixinmoney() > 0 && t.getTransactionId() == null) {
+            Map<String, String> orderResult = wXPayService
+                    .QueryOrderByOut_Trade_No(t.getOutTradeNo());
+            if (orderResult != null) {
+                t.setBankType(orderResult.get("bank_type"));
+                t.setTransactionId(orderResult.get("transaction_id"));
+                t.setFeeType(orderResult.get("fee_type"));
+                t.setDeviceInfo(orderResult.get("device_info"));
+                t.setTradeType(orderResult.get("trade_type"));
+                t.setTimeEnd(orderResult.get("time_end"));
+                t.setCashFee(Double.parseDouble(orderResult.get("cash_fee")
+                        .trim()));
+                tradeDao.update(t);
+                return false;
+            } else {
+                tradeDao.deleteById(t.getId());
+                Map<String, Object> paraMap = new HashMap<String, Object>();
+                paraMap.put("tradeId", t.getId());
+                List<TradeDetail> list = tradeDetailDao.find(paraMap);
+                if (list != null) {
+                    for (TradeDetail td : list) {
+                        tradeDetailDao.deleteById(td.getId());
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
     public Trade get(Serializable id) {
-        return tradeDao.get(id);
+        Trade t = tradeDao.get(id);
+        if (tradeFilterOne(t)) {
+            return null;
+        }
+        return t;
     }
 
     public void insert(Trade trade) {
@@ -83,8 +143,55 @@ public class TradeServiceImpl implements TradeService {
 
     @Override
     public TradeVO view(String tradeId) {
+        return tradeFilterMore(tradeId);
+    }
 
-        return tradeDao.view(tradeId);
+    private TradeVO tradeFilterMore(String tradeId) {
+        TradeVO t = tradeDao.view(tradeId);
+        if (t.getWeixinmoney() > 0 && t.getTransactionId() == null) {
+            Map<String, String> orderResult = wXPayService
+                    .QueryOrderByOut_Trade_No(t.getOutTradeNo());
+            if (orderResult != null) {
+                Trade tr = new Trade();
+                tr.setId(t.getId());
+                tr.setOutTradeNo(t.getOutTradeNo());
+                tr.setPayUserId(t.getPayUserId());
+                tr.setPayUserName(t.getPayUserName());
+                tr.setPayUserOpenid(t.getPayUserOpenid());
+                tr.setBankType(orderResult.get("bank_type"));
+                tr.setTransactionId(orderResult.get("transaction_id"));
+                tr.setFeeType(orderResult.get("fee_type"));
+                tr.setDeviceInfo(orderResult.get("device_info"));
+                tr.setTradeType(orderResult.get("trade_type"));
+                tr.setTimeEnd(orderResult.get("time_end"));
+                tr.setCashFee(Double.parseDouble(orderResult.get("cash_fee")
+                        .trim()) / 100);
+                tr.setCategory(t.getCategory());
+                tr.setTotalFee(t.getTotalFee());
+                tr.setFmoney(t.getTotalFee());
+                tr.setWeixinmoney(t.getWeixinmoney());
+                tr.setCountmoney(t.getCountmoney());
+                tr.setXianjinPay(t.getXianjinPay());
+                tr.setState(Integer.parseInt(t.getState()));
+                tr.setAreaId(t.getAreaId());
+                tr.setAreaName(t.getAreaName());
+                tr.setName(t.getName());
+                tradeDao.update(tr);
+                t = tradeDao.view(tradeId);
+            } else {
+                tradeDao.deleteById(t.getId());
+                Map<String, Object> paraMap = new HashMap<String, Object>();
+                paraMap.put("tradeId", t.getId());
+                List<TradeDetail> list = tradeDetailDao.find(paraMap);
+                if (list != null) {
+                    for (TradeDetail td : list) {
+                        tradeDetailDao.deleteById(td.getId());
+                    }
+                }
+                t = null;
+            }
+        }
+        return t;
     }
 
     @Override
